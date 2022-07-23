ae8f41c09ed7359dfff7b61e2e6266de300ebd6c
Ankush Bhardwaj <40923324+ankushbhardwxj@users.noreply.github.com>
2022-07-23 18:01:36 +0530

    resolvers for Post graphql done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,12 @@
 import { MikroORM, RequiredEntityData } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -17,9 +18,10 @@ const main = async () => {
   const app = express();
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false,
     }),
+    context: () => ({ em: fork }),
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
